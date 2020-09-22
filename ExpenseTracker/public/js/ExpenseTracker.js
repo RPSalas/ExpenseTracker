d0c53e656de6dbb229577463ffec7cf1797f8b95
Ryan Salas <rpsalas@hotmail.com>
2020-09-22 13:59:26 -0700

    Created and styled home page with functioning button to show form

---
+++ ExpenseTracker/public/js/ExpenseTracker.js
@@ -0,0 +1,8 @@
+function showForm() {
+    var showf = document.getElementById("ExpenseForm");
+    if (showf.style.display === "none") {
+      showf.style.display = "block";
+    } else {
+      showf.style.display = "none";
+    }
+}
